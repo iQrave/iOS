9ffde91e3c7e0b805d3937fb6fa9507eb3ee5b2b
Andrew Rauh <andrew@andrewrauh.com>
2012-06-07 22:25:10 -0400

    setup

---
+++ iQrave/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  iQrave
+//
+//  Created by Andrew Rauh on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ iQrave/FirstViewController.m
@@ -0,0 +1,34 @@
+//
+//  FirstViewController.m
+//  iQrave
+//
+//  Created by Andrew Rauh on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
