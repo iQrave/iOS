62c3853128ca5f4f59979076727f6901bcb78505
Andrew Rauh <andrew@andrewrauh.com>
2012-06-14 21:58:55 -0400

    added custom tabbarbutton

--- iQrave/CustomTabBarController.m
+++ iQrave/CustomTabBarController.m
@@ -27,8 +27,45 @@
 {
     [self setSelectedViewController:[self.viewControllers objectAtIndex:1]];
     [super viewDidLoad];
+    
+    [self addCenterButtonWithImage:[UIImage imageNamed:@"capture-button.png"] highlightImage:nil];
+
 	// Do any additional setup after loading the view.
 }
+
+
+-(void) addCenterButtonWithImage:(UIImage*)buttonImage highlightImage:(UIImage*)highlightImage
+{
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
+    button.frame = CGRectMake(0.0, 0.0, buttonImage.size.width, buttonImage.size.height);
+    
+
+    [button setBackgroundImage:buttonImage forState:UIControlStateNormal];
+    [button setBackgroundImage:highlightImage forState:UIControlStateHighlighted];
+    [button addTarget:self action:@selector(buttonWasPressed) forControlEvents:UIControlEventTouchUpInside];
+
+    CGFloat heightDifference = buttonImage.size.height - self.tabBar.frame.size.height;
+    if (heightDifference < 0)
+        button.center = self.tabBar.center;
+    else
+    {
+        CGPoint center = self.tabBar.center;
+        center.y = center.y - heightDifference/2.0;
+        button.center = center;
+    }
+    
+    [self.view addSubview:button];
+}
+
+
+- (void) buttonWasPressed {
+    
+    
+    
+}
+
+
 
 - (void)viewDidUnload
 {
